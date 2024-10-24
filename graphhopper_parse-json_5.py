d8665d6c00806bc94ae552340c159d56aa2b6b47
Mark Jearald Agraviador <agraviador.markjearalds@gmail.com>
2024-10-24 02:38:13 +0000

    added vehicle mode

--- graphhopper_parse-json_5.py
+++ graphhopper_parse-json_5.py
@@ -61,6 +61,20 @@ def geocoding (location, key):
 
 #User Input for Locations
 while True:
+ print("\n+++++++++++++++++++++++++++++++++++++++++++++")
+ print("Vehicle profiles available on Graphhopper:")
+ print("+++++++++++++++++++++++++++++++++++++++++++++")
+ print("car, bike, foot")
+ print("+++++++++++++++++++++++++++++++++++++++++++++")
+ profile=["car", "bike", "foot"]
+ vehicle = input("Enter a vehicle profile from the list above: ")
+ if vehicle == "quit" or vehicle == "q":
+   break
+ elif vehicle in profile:
+   vehicle = vehicle
+ else:
+   vehicle = "car"
+   print("No valid vehicle profile was entered. Using the car profile.")
  loc1 = input("Starting Location: ")
  if loc1 == "quit" or loc1 == "q":
    break
@@ -74,13 +88,13 @@ while True:
  if orig[0] == 200 and dest[0] == 200: 
    op="&point="+str(orig[1])+"%2C"+str(orig[2])
    dp="&point="+str(dest[1])+"%2C"+str(dest[2])
-   paths_url = route_url + urllib.parse.urlencode({"key":key}) + op + dp
+   paths_url = route_url + urllib.parse.urlencode({"key":key, "vehicle":vehicle}) + op + dp
    paths_status = requests.get(paths_url).status_code
    paths_data = requests.get(paths_url).json()
    print("Routing API Status: " + str(paths_status) + "\nRouting API URL:\n" +
    paths_url) 
    print("=================================================")
-   print("Directions from " + orig[3] + " to " + dest[3])
+   print("Directions from " + orig[3] + " to " + dest[3]+ " by " + vehicle)
    print("=================================================")
    if paths_status == 200:
       miles = (paths_data["paths"][0]["distance"])/1000/1.61
